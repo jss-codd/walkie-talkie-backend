827f56c837f9a7fdbf9adee9e3827bb90c2047c6
sachin <sachin.mandloi@webiwork.com>
2024-07-23 16:16:05 +0530

    changes

--- index.js
+++ index.js
@@ -21,11 +21,13 @@ app.get('/', (req, res) => {
 });
 
 let userMap = new Map();
-let locations = []
+let locations = [];
+const roomName = 'room';
 
 // const sockets = await io.in(routeid).fetchSockets(); => To get socket user in the room
 
-io.on('connection', (socket) => {
+
+io.on('connection', async (socket) => {
     console.log('A user connected', socket.id);
 
     socket.on("roomJoin", async (roomId) => {
@@ -57,7 +59,7 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('offer', async (data) => {
+    socket.on('offer1', async (data) => {
         console.log('-----------offer');
 
         // get details of caller & current location
@@ -77,12 +79,12 @@ io.on('connection', (socket) => {
         }
     });
     
-    socket.on('answer', (data) => {
+    socket.on('answer1', (data) => {
         console.log('---------answer')
         socket.broadcast.emit('answer', data);
     });
     
-    socket.on('candidate', (data) => {
+    socket.on('candidate1', (data) => {
         console.log('------------candidate')
         socket.broadcast.emit('candidate', data);
     });
@@ -95,7 +97,13 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('User disconnected', socket.id);
 
+        socket.broadcast.emit('user-disconnected', socket.id);
+
         socket.broadcast.emit('leaveCall', {});
+
+        socket.to(roomName).emit("endOfCall", {
+            callerId: socket.id,
+        });
 
         const userInfo = userMap.get(socket.id);
 
@@ -107,6 +115,104 @@ io.on('connection', (socket) => {
             io.emit('receiveLocation', locations);
         }
     });
+
+    //---------------------------------------
+
+      socket.on('join', async (room, callback) => {
+        console.log(room, socket.id, 'join');
+
+        socket.join(room);
+        
+        socket.to(room).emit('user-joined', socket.id);
+      });
+
+      socket.on('leave', room => {
+        console.log(room, socket.id, 'leave')
+        socket.leave(room);
+        socket.to(room).emit('user-left', socket.id);
+        socket.broadcast.emit('user-disconnected', socket.id);
+      });
+    
+      socket.on('offer', (room, offer) => {
+        socket.to(room).emit('offer', socket.id, offer);
+      });
+    
+      socket.on('answer', (room, answer) => {
+        socket.to(room).emit('answer', socket.id, answer);
+      });
+    
+      socket.on('candidate', (room, candidate) => {
+        socket.to(room).emit('candidate', socket.id, candidate);
+      });
+
+      socket.on('calling', () => {
+        socket.broadcast.emit('calling');
+      });
+      
+      //---------------------------------------
+
+      socket.on('joinRoom', async (payload, callback) => {
+        let roomId = payload.roomId;
+
+        socket.join(roomId);
+        
+        if (payload.userType === "caller") {
+            socket.join(roomId);
+
+            const findCaller = locations.filter(d => d.socketId == socket.id);
+
+            if(findCaller && findCaller.length > 0) {
+              const calllerId = findCaller[0]['id'];
+
+              const location = {lat: findCaller[0]['latitude'] || 0, lng: findCaller[0]['longitude'] || 0};
+
+              const resCaller = await Devices.findOne({ where: { id: calllerId } });
+
+              if(resCaller != null) {
+                socket.broadcast.emit('calling', resCaller.name, location);
+              }
+            }
+        } else if (payload.userType === "receiver") {
+            socket.join(roomId);
+            socket.to(roomId).emit("requestToJoin", {
+            callerId: socket.id,
+            });
+        }
+      });
+
+      socket.on('teacherLive', async (payload) => {
+        let rtcMessage = payload.rtcMessage;
+        let roomId = payload.roomId;
+
+        socket.to(roomId).emit('receiveTeacherCall', { rtcMessage: rtcMessage, callerId: socket.id});
+      });
+
+      socket.on('ICEcandidate', async (payload) => {
+        let calleeId = payload.calleeId;
+        let rtcMessage = payload.rtcMessage;
+
+        socket.to(calleeId).emit("ICEcandidate", {
+            sender: socket.id,
+            rtcMessage: rtcMessage,
+        });
+      });
+
+      socket.on('endCall', async (payload) => {
+        let roomId = payload.roomId;
+        socket.to(roomId).emit("endOfCall", {
+          callerId: socket.id,
+        });
+      });
+
+      socket.on('answerCall', async (payload) => {
+        let callerId = payload.callerId;
+        let rtcMessage = payload.rtcMessage;
+        
+        socket.to(callerId).emit("callAnswered", {
+            callee: socket.id,
+            rtcMessage: rtcMessage,
+        });
+      });
 });
 
 app.use('/resources',express.static(__dirname + '/myuploads'));
--- utils/Constants.js
+++ utils/Constants.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "https://square-immune-hound.ngrok-free.app";
+const SERVER_URL = "https://current-fish-manually.ngrok-free.app";
 // const SERVER_URL = "https://77a8-125-99-173-186.ngrok-free.app";
 
 const errorMessage = { otpRetryExceeded: "Maximum OTP retry limit is exceeded!", account404: "Account is not exist in our record!", wrongOTP: "Incorrect PIN Entered!", blockedAccount: "This account is blocked! Please contact our support for unblock it.", uploadFile: "Please upload a file", positionError: "Failed to send! Can't measure current location.", common: "Something went wrong, Try again later.", mobileInput: "Invalid mobile number!", invalidOTP: "Invalid OTP!", invalidAction: "Invalid action!", otpSentExceeded: "You have already sent multiple OTP request! Wait for 10 minutes for next request.", }
